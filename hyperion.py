10ba5d11baf1ad89996c217ed40a37ca1975204b
Denis <gaphex@gmail.com>
2015-06-25 13:31:31 +0300

    DBScan clusterization for Hyperion

--- hyperion.py
+++ hyperion.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = 'denisantyukhov'
 
 import re
@@ -11,7 +12,7 @@ import urllib2
 import Stemmer
 import sqlite3
 import operator
-import pymorphy2
+import collections
 import numpy as np
 import pandas as pd
 from sys import stdout
@@ -19,37 +20,34 @@ from lshash import LSHash
 from datetime import datetime
 import matplotlib.pyplot as plt
 from collections import Counter
+from sklearn.cluster import DBSCAN
 from scipy.sparse import csr_matrix, vstack
-from sklearn.metrics.pairwise import cosine_similarity
+
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Hyperion():
     def __init__(self, data):
         print 'Invoking Hyperion...'
-        self.morph = pymorphy2.MorphAnalyzer()
         self.stemmer = Stemmer.Stemmer('russian')
+        self.probe = []
         self.data = data
-
-    def prepare(self):
-        con = sqlite3.connect('tweetsSpring.db')
-        self.data = pd.read_sql("SELECT * from tweets",con)
-        self.backup = self.preprocess()
-
+        
     def processContents(self, myText):
         myText = re.sub(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}     /)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', '', myText)
         words = [word for word in re.findall(r'(?u)[@|#]?\w+', myText) if not word.startswith(('@','#'))]
         words = self.stemmer.stemWords(words)
         return words
-
+        
     def preprocess(self):
-
-        print 'Preprocessing...this may take a while'
+        
+        print 'Preprocessing...this will take a while'
         t0 = time.time()
         terms = []
         words = []
 
-        n = len(data.index)
+        n = len(self.data.index)
 
         for i in range(n):
             terms.append(self.processContents(self.data.content_lower[i]))
@@ -68,7 +66,7 @@ class Hyperion():
 
         trainingList = []
         for i in range(n):
-            trainingList.append(' '.join(data['terms'][i]))
+            trainingList.append(' '.join(self.data['terms'][i]))
 
         self.vectorizer = TfidfVectorizer(vocabulary = interestingVocab)
         self.tfidf = self.vectorizer.fit_transform(trainingList)  #finds the tfidf score with normalization
@@ -84,7 +82,7 @@ class Hyperion():
         if i == n-1:
             stdout.write("\r100%")
             print("\r\n")
-
+        
     def median(self,lst):
         return np.median(np.array(lst))
 
@@ -104,16 +102,14 @@ class Hyperion():
             return finalurl
         except:
             pass
-
+        
     def setup(self, vectorizer, tf_idf):
         self.vectorizer = vectorizer
         self.tfidf = tf_idf
         print 'Setup complete '
-
+        
 
     def findSimilarTweets(self, queryTweet, threshold, maxNumber = 0, log = False):
-
-        t0 = time.time()
 
         processedTweet = ' '.join(self.processContents(queryTweet))
         queryTweetRepresentation = self.vectorizer.transform([processedTweet])
@@ -124,142 +120,46 @@ class Hyperion():
         if maxNumber:
             totalMatchingTweets = min(totalMatchingTweets, maxNumber)
         indices = cosine_similarities.argsort()[::-1][:totalMatchingTweets]
-        elapsed_time = time.time() - t0
         if len(indices) > 25:
             print 'Query:', queryTweet
+            
         return indices
-
-    def findOut(self, cluster):
-        lst = list(self.data['content'][cluster])
-
-        redirects = []
-        keywords = []
-
-        urls = self.extract_urls(lst)
-        for url in urls:
-            redirects.append(self.resolve_url(url))
-
-        corpus = ' '.join(lst).lower()
-        corpus = re.sub(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}     /)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', '', corpus)
-        words = []
-        tokenized = nltk.tokenize.word_tokenize(corpus)
-        for word in tokenized:
-            words.append(word)
-
-        fdist = nltk.FreqDist(words)
-        sortedDist = sorted(fdist.items(), key=operator.itemgetter(1),reverse=True)
-        moreThan3 = [x for x in sortedDist if len(x[0]) > 3 and x[1] > 3]
-        for i in moreThan3:
-            keywords.append(i[0])
-
-        return(keywords, redirects)
-
-    def resolve(self, masterCluster):
-        t0 = time.time()
-        k, r = self.findOut(masterCluster)
-
-        insta = []
-        swarm = []
-
-        print 'Found', len(r), 'anonymous links, investigating...'
-        for i, u in enumerate(r):
-            if u:
-                if 'instagram' in u:
-                    insta.append(u)
-                if 'swarmapp' in u:
-                    swarm.append(u)
-
-        print 'Resolving took', time.time()-t0
-        return ((k, insta, swarm))
-
-    def overlap(self, r1l, r1r, r1t, r1b, r2l, r2r, r2t, r2b):
-        #Overlapping rectangles overlap both horizontally & vertically
-        return self.range_overlap(r1l, r1r, r2l, r2r) and self.range_overlap(r1b, r1t, r2b, r2t)
-
-    def range_overlap(self, a_min, a_max, b_min, b_max):
-        #Neither range is completely greater than the other
-        return (a_min <= b_max) and (b_min <= a_max)
-
-    def assess(self, clusters):
-        data = []
-        for c in clusters:
-
-            xs = [t[0] for t in c]
-            ys = [t[1] for t in c]
-            z = zip(xs, ys)
-            n = len(z)
-
-            dx = max(xs) - min(xs)
-            dy = max(ys) - min(ys)
-
-            mx = self.median(xs)
-            my = self.median(ys)
-
-            s=(dx+0.001)*(dy+0.001)
-            score = n/s
-
-            data.append((z,n,score,dx,dy,mx,my))
-            data = filter(lambda a: a[1] > 5, data)
-
-        return data
-
-    def findUnique(self, stats):
-
-        for v in range(3):
-            for i in stats:
-                r1l = i[5]-1.2*i[3]
-                r1r = i[5]+1.2*i[3]
-                r1b = i[6]-1.2*i[4]
-                r1t = i[6]+1.2*i[4]
-                for j in stats:
-                    r2l = j[5]-1.2*j[3]
-                    r2r = j[5]+1.2*j[3]
-                    r2b = j[6]-1.2*j[4]
-                    r2t = j[6]+1.2*j[4]
-                    if self.overlap(r1l, r1r, r1t, r1b, r2l, r2r, r2t, r2b) and i!=j:
-                        if i[1] >= j[1]:
-                            try:
-                                stats.remove(j)
-                            except:
-                                pass
-                        else:
-                            try:
-                                stats.remove(i)
-                            except:
-                                pass
-
-        return stats
-
-    def performClusterisation(self, indices, thSp, thTm):
-
-        #Spatial Clustering
-        y = list(data['lat'][indices])
-        x = list(data['long'][indices])
-        tid = indices
-
+    
+    def filter(self, x, y, tid):
         z = filter(lambda a: a[0] != (55.753301, 37.619899), zip(zip(x,y), tid))
         x = []
         y = []
         tid = []
-
         for t in z:
             x.append(t[0][0])
             y.append(t[0][1])
             tid.append(t[1])
-
         crds = zip(x,y)
-        spUniques = self.findCluster(crds, tid, 34, thSp, thTm, 'space')
+        return crds, tid
+    
+    def performClusterisation(self, indices, thSp, thTm):
+        
+        #Spatial Clustering
+        y = list(self.data['lat'][indices])
+        x = list(self.data['long'][indices])
+        tid = indices
+        
+        crds, tid = self.filter(x, y, tid)
+
+        spUniques = self.findDBCluster(crds, tid, thSp, 5)
+        
         if len(spUniques):
             print '--------------------------------------------------------------'
-            print 'Found', len(spUniques), 'spatial clusters with a total of', sum([len(t[0]) for t in spUniques]), 'points among', len(indices)
+            print 'Found', len(spUniques), 'spatial clusters'
 
         for w in spUniques:
-            print len(w[0]), 'points at', w[3], w[4]
+            kw = ' '.join(self.extractKeywords(w['ids'])[:5])
+            print len(w['ids']), 'points at', w['xm'], w['ym'], kw
 
 
         #Temporal Clustering
         tmvc = []
-        for t in data['created_at'][indices]:
+        for t in self.data['created_at'][indices]:
             l = t.split(' ')
             date = l[0].split('-')
             time = l[1].split(':')
@@ -267,87 +167,126 @@ class Hyperion():
             timehash = int(time[0]) * 3600 + int(time[1]) * 60 + int(time[2])
             tmvc.append((datehash*400, timehash))
 
-        tpUniques = self.findCluster(tmvc, tid, 34, thSp, thTm, 'time')
-        dm = len(tpUniques)
+        tpUniques = self.findDBCluster(tmvc, tid, thTm, 5)
 
-        if dm:
+        if len(tpUniques):
             print '--------------------------------------------------------------'
-            print 'Found', dm, 'temporal clusters with a total of', sum([len(t[0]) for t in tpUniques]), 'points among', len(indices)
-
+            print 'Found', len(tpUniques), 'temporal clusters'
+        
 
         for w in tpUniques:
-            date = w[3]/400
-            time = w[4]
-            print len(w[0]), 'points at', int(date//30),'.',int(date%30),'--',int(time//3600),
-            print ':',int(time%3600//60),':',int(time%3600%60)
+            date = w['xm']/400
+            time = w['ym']
+            kw = ' '.join(self.extractKeywords(w['ids'])[:5])
+            print len(w['ids']), 'points at', int(date//30),'.',int(date%30),'--',int(time//3600),
+            print ':',int(time%3600//60),':',int(time%3600%60), kw
 
         print ''
         return ((spUniques, tpUniques))
+    
+    def extractKeywords(self, cluster):
+        keywords = []
+        corpus = ' '.join(list(self.data['content'][cluster]))
+        corpus = re.sub(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}     /)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', '', corpus)
+        words = []
+        tokenized = nltk.tokenize.word_tokenize(corpus)
+        for word in tokenized:
+            words.append(word)
+        #words = self.stemmer.stemWords(words)
 
-    def findCluster(self, vecs, tids, cln, thSp, thTm, mode):
+        fdist = nltk.FreqDist(words)
+        sortedDist = sorted(fdist.items(), key=operator.itemgetter(1),reverse=True)
+        moreThan3 = [x for x in sortedDist if len(x[0]) > 3 and x[1] > 3]
+        for i in moreThan3:
+            keywords.append(i[0])
 
-        maxn = 0
-        maxc = []
-        dim = len(vecs[0])
-        data = zip(vecs, tids)
+        return keywords
+    
+    def processURLs(self, cluster):
+        insta = []
+        swarm = []
+        redirects = []
+        lst = list(self.data['content'][cluster])
+        urls = self.extract_urls(lst)
+        for i in urls:
+            redirects.append(self.resolve_url(i))
 
+        print 'Found', len(redirects), 'anonymous links, investigating...'
+        for i, u in enumerate(redirects):
+            if u:
+                if 'instagram' in u:
+                    insta.append(u)
+                if 'swarmapp' in u:
+                    swarm.append(u)
 
-        if mode == 'space':
-            metric = "euclidean"
-            th = thSp
-            rat = 1
-        if mode == 'time':
-            metric = 'hamming'
-            th = thTm
-            rat = 1
+        return (insta, swarm)
 
-        lsh = LSHash(cln, dim)
-        for i in vecs:
-            lsh.index(i)
-
-        pivots = list(set(random.sample(vecs, int(rat*len(vecs)))))
-
-        for i in pivots:
-            mxc = []
-            dists = []
-            u = lsh.query(i, distance_func=metric)
-
-            for j in u:
-                dists.append(j[1])
-                mxc.append(j[0])
-
-            r = self.cover(dists, th)
-            maxc.append(mxc[0:r])
-
-
-        scr = self.assess(maxc)
-        rez = self.findUnique(scr)
+    def findDBCluster(self, crds, tids, epsilon, min):
 
         clusters = []
-        for u in rez:
-            cIDs = []
-            for t in u[0]:
-                for d in data:
-                    if t == d[0]:
-                        cIDs.append(d[1])
-            clusters.append((cIDs, u[3], u[4], u[5], u[6]))
+        rez = []
+        X = np.array(crds)
+        db = DBSCAN(eps=epsilon, min_samples=min).fit(X)
+        core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+        core_samples_mask[db.core_sample_indices_] = True
+        labels = db.labels_
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        unique_labels = set(labels)
+        colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+        for k, col in zip(unique_labels, colors):
+            if k == -1:
+                # Black used for noise.
+                col = 'k'
 
-        clusters = filter(lambda a: len(a[0]) > 6, clusters)
+            class_member_mask = (labels == k)
 
-        return (clusters)
+            xy = X[class_member_mask & core_samples_mask]
+            #plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=10)
+            if len(xy):
+                rez.append(xy)
+            xy = X[class_member_mask & ~core_samples_mask]
+            #plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=2)
 
-    def cover(self, dat, th):
-        sum = 0
-        t = 0
-        while sum < th*len(dat) and t < 0.5*len(dat):
-            sum += dat[t]
-            t += 1
-        return t
+        #plt.title('Estimated number of clusters: %d' % n_clusters_)
+        #plt.show()
+
+        u = zip (crds, tids)
+        for c in rez:
+            buf = []
+            for t in c:
+                for j in u:
+                    if tuple(t) == j[0]:
+                        buf.append(j)
+            clusters.append(list(set(buf)))
+
+        scored = []
+        for c in clusters:
+            scored.append(self.score(c))
+
+        scored = filter(lambda a: len(a['ids'])> min, scored)
+        scored.sort(key = lambda t: t['score'], reverse = True)
+
+        return scored
+    
+    def score(self, cluster):
+        processed = {'ids':[], 'xm':0, 'ym':0, 'score':0, 'num':0}
+        cluster = list(set(cluster))
+        if len(cluster):
+            xs = [t[0][0] for t in cluster]
+            ys = [t[0][1] for t in cluster]
+            ids = [t[1] for t in cluster]
+            
+            processed['ids'] = ids
+            processed['xm'] = hyperion.median(xs)
+            processed['ym'] = hyperion.median(ys)
+            processed['score'] = len(ids)*len(ids)/((max(xs)-min(xs)+0.01)*(max(ys)-min(ys)+0.01))
+            processed['num'] = len(ids)
+            
+        return processed
 
     def doQuery(self, query, th_NN, th_SP, th_TM):
         t0 = time.time()
         indices = self.findSimilarTweets(query, th_NN)
-        print self.data['content'][indices]
         print len(indices), 'points passed preprocessing'
 
         if len(indices):
@@ -355,6 +294,7 @@ class Hyperion():
             print "Query processing took", time.time()-t0, 'seconds'
 
         return t
+            
 
 def setupHyperion(data):
     hyperion = Hyperion(data)
